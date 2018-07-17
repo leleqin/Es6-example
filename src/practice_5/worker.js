d917c00c127f46fdec996c597d1acb7a7e92758f
leleqin <602568224@qq.com>
2018-07-17 17:50:54 +0800

    完成practice_6

--- src/practice_5/worker.js
+++ src/practice_5/worker.js
@@ -1,7 +1,10 @@
 import Person from "./person";
 
 export default class Worker extends Person{
+    constructor(name,age){
+        super(name,age);
+    }
     introduce(){
-        return super.introduce() + " I am a Worker. I have a job.";
+        return super.introduce() + " I am a Teacher. I have a job.";
     }
 }
