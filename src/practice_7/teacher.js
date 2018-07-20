483a111453941ae6ed55c96e9d24d596100c3921
leleqin <602568224@qq.com>
2018-07-20 18:02:41 +0800

    完成practice_7

--- src/practice_7/teacher.js
+++ src/practice_7/teacher.js
@@ -1,1 +1,19 @@
+import Person from "./person";
 
+export default class Teacher extends Person{
+    constructor(name, age, klass){
+        super(name, age);
+        this.klass = klass;
+    }
+
+    introduce(){
+        return `${super.introduce()} I am a Teacher. I teach ${this.klass === undefined ? 'No Class' : 'Class ' + this.klass.number}.`;
+    }
+
+    introduceWith(student){
+        let teached = false;
+        if(this.klass !== undefined && this.klass.number === student.klass.number)
+            teached = true;
+        return `${super.introduce()} I am a Teacher. ${teached === true ? 'I teach ' + student.name + '.' : 'I don\'t teach ' + student.name + '.'}`;
+    }
+}
