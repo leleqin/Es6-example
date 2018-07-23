028578e913b20d4493e94d7ee7914fbf8c8f9522
leleqin <602568224@qq.com>
2018-07-23 10:48:41 +0800

    完成practice_9

--- src/practice_9/class.js
+++ src/practice_9/class.js
@@ -1,1 +1,19 @@
+export default class Class {
+    constructor(number){
+        this.number = number;
+    }
 
+    getDisplayName(){
+        return `Class ${this.number}`;
+    }
+
+    assignLeader(student){
+        if(student.klass === this)
+            this.leader = student;
+        console.log('It is not one of us.');
+    }
+
+    appendMember(student){
+        student.setClass(this);
+    }
+}
--- src/practice_9/student.js
+++ src/practice_9/student.js
@@ -1,1 +1,21 @@
+import Person from "./person";
 
+export default class Student extends Person{
+    constructor(id, name, age, clazz){
+        super(id, name, age);
+        this.klass = clazz;
+    }
+
+    introduce(){
+        let leaderStr = `I am at Class ${this.klass.number}.`;
+        if(this.klass.leader)
+            leaderStr = `I am Leader of Class ${this.klass.number}.`;
+        let result = `${super.introduce()} I am a Student. ` + leaderStr;
+        return result;
+    }
+
+    setClass(clazz){
+        this.klass = clazz;
+    }
+
+}
