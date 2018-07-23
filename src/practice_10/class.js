60de69a34d998b2f2c3ba6f069dfb9ffa364a2ef
leleqin <602568224@qq.com>
2018-07-23 10:56:36 +0800

    完成practice_10

--- src/practice_10/class.js
+++ src/practice_10/class.js
@@ -1,1 +1,30 @@
+export default class Class {
+    constructor(number){
+        this.number = number;
+        this.students = [];
+    }
 
+    getDisplayName(){
+        return `Class ${this.number}`;
+    }
+
+    assignLeader(student){
+        if(student.klass === this)
+            this.leader = student;
+        console.log('It is not one of us.');
+    }
+
+    appendMember(student){
+        if(!this.isIn(student)) {
+            this.students.push(student);
+            student.setClass(this);
+        }
+    }
+
+    isIn(student){
+        if(this.students){
+            return this.students.find((elem) => elem.equal(student)) ? true : false;
+        }
+    }
+
+}
--- src/practice_10/teacher.js
+++ src/practice_10/teacher.js
@@ -1,1 +1,26 @@
+import Person from "./person";
 
+export default class Teacher extends Person{
+    constructor(id, name, age, klasses){
+        super(id, name, age);
+        this.klasses = klasses ? klasses : [];
+    }
+
+    introduce(){
+        let result = `${super.introduce()} I am a Teacher. `;
+        result += this.klasses.length != 0 ? `I teach Class ${this.klasses.map((klass) => klass.number).join(', ')}.` : `I teach No Class.`;
+        return result;
+    }
+
+    introduceWith(student){
+        let teached = false;
+        if(this.klasses.find(klass => klass.isIn(student)))
+            teached = true;
+        return `${super.introduce()} I am a Teacher. ${teached ? 'I teach ' + student.name + '.' : 'I don\'t teach ' + student.name + '.'}`;
+    }
+
+    isTeaching(student){
+        return this.klasses.find(klass => klass.isIn(student)) ? true : false;
+    }
+
+}
