fdb434fa06ee41018cd9f18f1f10b508e8ab3977
leleqin <602568224@qq.com>
2018-07-23 10:42:23 +0800

    完成practice_8

--- src/practice_8/student.js
+++ src/practice_8/student.js
@@ -1,1 +1,15 @@
+import Person from "./person";
 
+export default class Student extends Person{
+    constructor(id,name,age,klass){
+        super(id,name,age);
+        this.klass = klass;
+    }
+    introduce(){
+        let leaderStr = `I am at Class ${this.klass.number}.`;
+        if(this.klass.leader)
+            leaderStr = `I am Leader of Class ${this.klass.number}.`;
+        let result = `${super.introduce()} I am a Student. ` + leaderStr;
+        return result;
+    }
+}
--- src/practice_8/teacher.js
+++ src/practice_8/teacher.js
@@ -1,1 +1,21 @@
+import Person from "./person";
 
+export default class Teacher extends Person{
+    constructor(id, name, age, klass){
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    introduce(){
+        let result = `${super.introduce()} I am a Teacher. `;
+        result += this.klass ? `I teach Class ${this.klass.number}.` : `I teach No Class.`;
+        return result;
+    }
+
+    introduceWith(student){
+        let teached = false;
+        if(this.klass !== undefined && this.klass.number === student.klass.number)
+            teached = true;
+        return `${super.introduce()} I am a Teacher. ${teached === true ? 'I teach ' + student.name + '.' : 'I don\'t teach ' + student.name + '.'}`;
+    }
+}
